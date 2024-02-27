9d0b782a243a0ce8eb08a1e01c774de1f981e11f
Alexey Oplachko <xirenec1@gmail.com>
2024-02-27 11:00:52 +0200

    feat: export as pcap
    fix: export as pcap
    
    # Conflicts:
    #	ngx-flow/package.json
    #	package.json

--- src/components/SimplePanel.tsx
+++ src/components/SimplePanel.tsx
@@ -4,15 +4,25 @@ import { SimpleOptions } from 'types';
 import { css, cx } from '@emotion/css';
 import './../../ngx-flow/widget/ngx-flow.js';
 import ReactJson from 'react-json-view';
+import { DateTime } from "luxon";
+// @ts-ignore
+import packets from '../libs/packets.js';
+// @ts-ignore
+import { configure } from 'pcap-generator'
+import { Buffer } from 'buffer'
+const ip = packets.ipPacket
+const udp = packets.udpPacket
+const tcp = packets.tcpPacket
 
 import {
     Button,
     MultiSelect,
     Collapse,
-    // AsyncSelect,
     Modal,
     useStyles2,
-    useTheme2
+    useTheme2,
+    Dropdown,
+    Menu
 } from '@grafana/ui';
 
 
@@ -202,6 +212,9 @@ function formattingDataAndSortIt(data: any, sortType = 'none') {
 
 }
 
+
+
+
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }: any) => {
     const [flowData, setFlowData] = React.useState({ actors: [], data: [] });
     const [modalIsOpen, setModalIsOpen] = React.useState(false);
@@ -211,13 +224,28 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }: a
     const onModalClose = () => {
         setModalIsOpen(false);
     };
+    // Export .txt
     useEffect(() => {
         const handler = (e: any) => {
 
             const [serie]: any = (data as any)?.series || [];
             const fields = serie?.fields || [];
             const lineField = fields.find((i: any) => i.name === 'Line') ?? [];
-            const exportText = lineField?.values.join('\n');
+            const exportText = lineField?.values.map((i: string, index: number) => {
+                let dt = DateTime.fromMillis(fields[0]?.values[index]?.timestamp).toISO()
+                dt = dt?.replace('+', '000+') ?? '';
+                if (fields[0]?.values[index]?.type !== 'sip') {
+                    i = '';
+                }
+                let proto = 'proto:'
+                if (i.includes('UDP')) {
+                    proto += "UDP"
+                } else if (i.includes('TCP')) {
+                    proto += "TCP"
+                }
+                console.log(proto)
+                return `${proto} ${dt} ${fields[0]?.values[index]?.src_ip} ---> ${fields[0]?.values[index]?.dst_ip} \n\n${i}`
+            }).join('\n');
             // Create element with <a> tag
             const link = document.createElement("a");
 
@@ -228,25 +256,96 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }: a
             link.href = URL.createObjectURL(file);
 
             // Add file name
-            link.download = "sample.txt";
-            console.log(link)
-            // Add click event to <a> tag to save file.
+            const date = new Date();
+            link.download = `${date.toUTCString()}.txt`;
             link.click();
-            URL.revokeObjectURL(link.href);
-            document.removeChild(link)
-            // console.log(exportText)
         }
         document.addEventListener('export-flow-as-text', handler);
         return () => {
             document.removeEventListener('export-flow-as-text', handler)
         }
     }, [data])
+    // Export .pcap
+    useEffect(() => {
+        const handler = (e: any) => {
+
+            const [serie]: any = (data as any)?.series || [];
+            const fields = serie?.fields || [];
+            const lineField = fields.find((i: any) => i.name === 'Line') ?? [];
+            const packets2 = lineField?.values.map((field: string, index: number) => {
+
+                const labels = fields[0]?.values[index]
+                let proto = ''
+                if (field.includes('UDP')) {
+                    proto += "UDP"
+                } else if (field.includes('TCP')) {
+                    proto += "TCP"
+                }
+                const fieldObj = {
+                    data: field,
+                    srcIp: labels.src_ip,
+                    dstIp: labels.dst_ip,
+                    srcPort: labels.src_port,
+                    dstPort: labels.dst_port,
+                    ts: fields[0]?.values[index]?.timestamp,
+                    proto: proto === 'UDP' ? 17 : 6,
+                    type: fields[0]?.values[index]?.type,
+                    trafficClass: 0,
+                    flowLabel: 0,
+                    nextHeader: proto === 'UDP' ? 17 : 6
+                }
+                const ipv4_regex = /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/gm;
+
+                const packet_data = proto === 'UDP' ? udp.encode({
+                    sourcePort: fieldObj.srcPort,
+                    destinationPort: fieldObj.dstPort,
+                    data: Buffer.from(fieldObj.data)
+                }) : tcp.encode({
+                    sourcePort: fieldObj.srcPort,
+                    destinationPort: fieldObj.dstPort,
+                    data: Buffer.from(fieldObj.data)
+                })
+                let ip_packet = ip.encode({
+                    version: ipv4_regex.test(fieldObj.srcIp) ? 4 : 6,
+                    protocol: fieldObj.proto,
+                    sourceIp: fieldObj.srcIp,
+                    destinationIp: fieldObj.dstIp,
+                    data: packet_data,
+                    ttl: 112,
+                    nextHeader: fieldObj.nextHeader,
+                    trafficClass: fieldObj.trafficClass,
+                    flowLabel: fieldObj.flowLabel
+                })
+                return {
+                    timestamp: fieldObj.ts,
+                    buffer: ip_packet,
+                    type: fields[0]?.values[index]?.type
+                }
+            }).filter((packet: any) => packet.type === 'sip')
+            const generator = configure({ Buffer: Buffer })
+            const pcapFile = generator(packets2)
+            const blob = new Blob([pcapFile], { type: 'application/vnd.tcpdump.pcap' });
+            // // Create element with <a> tag
+            const link = document.createElement("a");
+
+            // Add file content in the object URL
+            link.href = URL.createObjectURL(blob);
+
+            // Add file name
+            const date = new Date();
+            link.download = `${date.toUTCString()}.pcap`;
+            link.click();
+
+        }
+        document.addEventListener('export-flow-as-pcap', handler);
+        return () => {
+            document.removeEventListener('export-flow-as-pcap', handler)
+        }
+    }, [data])
     const styles = useStyles2(getStyles);
     React.useEffect(() => {
         const [serie]: any = (data as any)?.series || [];
         const fields = serie?.fields || [];
-        console.log({ options })
-        console.log(serie)
         if (fields) {
             const [firsField]: any = fields;
             const sortData = formattingDataAndSortIt(fields, options.sortoption);
@@ -297,6 +396,28 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }: a
     // console.log(useTheme2());
     const themeName: string = useTheme2().name;
     const flowDataJSON = JSON.stringify(flowData);
+    const menu = (
+        <Menu>
+            <Menu.Item
+                onClick={() => {
+                    document.dispatchEvent(new CustomEvent('export-flow-as-png'));
+                }}
+                label='Export flow as PNG'
+            ></Menu.Item>
+            <Menu.Item
+                onClick={() => {
+                    document.dispatchEvent(new CustomEvent('export-flow-as-json'));
+                }}
+                label='Export flow as JSON'
+            ></Menu.Item>
+            <Menu.Item
+                onClick={() => {
+                    document.dispatchEvent(new CustomEvent('export-flow-as-pcap'));
+                }}
+                label='Export flow as PCAP'
+            ></Menu.Item>
+        </Menu>
+    )
     return (
         <div
             className={cx(
@@ -304,9 +425,27 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }: a
                 css`
           width: ${width}px;
           height: ${height}px;
+          position: relative;
         `
             )}
         >
+            {data?.request?.app === 'dashboard' && (
+                // <span >
+                <Dropdown overlay={menu}>
+
+                    <Button className={cx(
+                        css`
+              position: absolute;
+              top: 15px;
+              right: 15px;
+              border: 1px solid ${themeName === 'Dark' ? 'hsla(240, 18.6%, 83.1%, 0.12)' : 'hsla(210, 12.2%, 16.1%, 0.12)'};
+              border-radius: 2px;
+              background-color: ${themeName === 'Dark' ? 'hsla(0, 0%, 0%, 0.5)' : 'hsla(0, 0%, 100%, 0.5)'};
+              z-index: 2;
+            `)} icon="bars" fill="text" variant="secondary" />
+                </Dropdown>
+                // </span>/
+            )}
             {/* <pre>{JSON.stringify(flowData)}</pre> */}
             {/* <FlowMemo flowData={flowData} themeName={themeName} /> */}
             <ngx-flow-out data-flow={flowDataJSON} theme={themeName} />
